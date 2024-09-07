433bdb8e2e10e2be3733a506915fa171e7b508be
Snyper10 <133409830+Snyper10@users.noreply.github.com>
2024-09-07 11:42:42 +0530

    Create apikey.py

---
+++ apikey.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import openai
+import requests
+import os
+from apikey import OPENAI_API_KEY, FMP_API_KEY
+openai.api_key = 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
